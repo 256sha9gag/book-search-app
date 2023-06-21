da9b7ab8e679c69baf1241bb4a258839e0c9cb8d
gag <256sha9gag@gmail.com>
2023-06-21 21:21:27 +0300

    feat: create global and baseTheme

---
+++ src/styles/baseTheme.ts
@@ -0,0 +1,12 @@
+import { type DefaultTheme } from 'styled-components'
+
+const baseTheme: DefaultTheme = {
+  colors: {
+    main: 'skyblue',
+    secondary: 'gray',
+    ligth: 'white',
+    dark: 'gray'
+  }
+}
+
+export default baseTheme
