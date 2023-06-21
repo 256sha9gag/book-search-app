00bbf3c6286308f819b8e78409398f27cd9fe169
gag <256sha9gag@gmail.com>
2023-06-21 21:19:15 +0300

    types: add

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
+const App: React.FC = (): JSX.Element => {
   return (
     <div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import reportWebVitals from './reportWebVitals'
+import { ThemeProvider } from 'styled-components'
+
+import baseTheme from './styles/baseTheme'
+import GlobalStyles from './styles/global'
+import App from './App'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+)
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={baseTheme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
-);
+)
 
-reportWebVitals();
+reportWebVitals()
