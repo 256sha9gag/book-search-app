e08e16fb962b48ee8bd88ebd31ea8135a49a1113
gag <256sha9gag@gmail.com>
2023-06-22 22:10:45 +0300

    feat: add layout header

---
+++ src/styles/components.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+export const HeaderWrapper = styled.header`
+  display: flex;
+  flex-direction: column;
+  background: url(${({ theme }) => theme.background.image}) no-repeat 50%;
+`
+
+export const Title = styled.h1`
+  font: 32px 'MontserratRegular';
+  padding: 30px 0;
+  text-align: center;
+  color: ${({ theme }) => theme.colors.ligth};
+`
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`
+
+export const InputWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Input = styled.input`
+  font: 16px 'MontserratRegular';
+  flex-basis: 40%;
+  padding: 10px;
+  color: ${({ theme }) => theme.colors.secondary};
+  border: none;
+  border-radius: 5px;
+`
