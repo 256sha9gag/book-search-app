38546701a32b2598377637f5f84981bc53308566
gag <256sha9gag@gmail.com>
2023-06-23 18:45:58 +0300

    feat: add handles

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { type FormEvent, useState, useRef } from 'react'
 import { Form, Input, InputWrapper } from '../styles/components'
 import SearchButton from './SearchButton'
+import Dropdown from './Dropdown'
 
 const SearchForm: React.FC = (): JSX.Element => {
+  const [searchValue, setSearchValue] = useState<string>('')
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault()
+    console.log(searchValue)
+  }
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setSearchValue(e.target.value)
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <InputWrapper>
-        <Input />
-        <SearchButton />
+        <Input
+          placeholder='Enter book title or author...'
+          ref={inputRef}
+          type="text"
+          name="search"
+          onChange={handleOnChange}
+          value={searchValue}
+        />
+        <SearchButton
+        />
       </InputWrapper>
+      <Dropdown />
     </Form>
   )
 }
